b8d6c4be178521d5f67f8f9578680a2e6ddcc487
Andrew Cui <andrewcui1029@gmail.com>
2022-04-20 15:16:23 -0700

    try

---
+++ econ_142_Project.R
@@ -0,0 +1,14 @@
+---
+title: 'Econ 142 Final Project'
+author: "Andrew Cui, Max Siu"
+
+date: ""
+output:
+  html_document: default
+  header-includes: usepackage{color}
+fontsize: 12
+margin: 1
+---
+<!--
+This part is a comment: it won't show in your output. You can use comments to remind you to change the name or the lab number, or a version number. You should use lots of them.  Some excellent economists say that about one quarter of your programs should be comments. Comments are most important in your blocks of code (the grey part of the RMarkdown document)
+-->
